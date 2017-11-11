c144e8f64e391a15b3d5ffef1de4e43f6a2c04c1
developertqw2017 <2144799613@qq.com>
2017-11-11 16:08:43 +0800

    xiufu

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import date, datetime
-from polls.models import playground,ticket,project
+from polls.models import playground,ticket,project,userBaseInfo,userticket,Comment,entertainment
 import json
 def search_playground(request,name):
     sear = playground.objects.get(Pname=name)
@@ -12,7 +12,7 @@ def search_playground(request,name):
     return HttpResponse(resp)
 
 def search_ticket(request,name):
-    sear = playground.objects.get(Pname="wuhanhuanlegu")
+    sear = playground.objects.get(Pname=name)
     qq = ticket.objects.get(Tid=sear.Pid)
     qq.__delattr__('Tdatetime')
     qq.__delattr__('_state')
@@ -30,3 +30,13 @@ def search_comment(request,name):
     sear_comment = sear.__delattr__('_state')
     search_comment = search_comment.__delattr__('Ctime')
     return HttpResponse(json.dumps(search_comment.__dict__))
+
+def search_userticket(request,name):
+    sear = userBaseInfo.objects.get(Uname=name)
+    sear_ticket = userticket.objects.filter(Uid = sear.Uid)
+    sear_ticket = [st.__delattr__('_state') for st in sear_ticket]
+    sear_ticketid = [ticket.objects.get(Tid = st.Uticket) for st in sear_ticket]
+    sear_ticketinfo = [st.__delattr__('_state') for st in sear_ticketid]
+    resp1 = json.dumps(sear_ticket.__dict__)
+    resp2 = json.dumps(sear_ticketinfo.__dict__)
+    return HttpResponse({resp1,resp2})
