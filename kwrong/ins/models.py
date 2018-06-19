89b4b24a29005f56ae07e92be353e8801f3d6d4f
ubuntu <ubuntu@localhost.localdomain>
2018-06-19 22:13:12 +0800

    ins add

---
+++ kwrong/ins/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='ins_userprofile'
+    )
